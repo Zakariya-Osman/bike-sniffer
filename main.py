18164f11e35c2b8040c1b69b08df0f6ecd914dd0
Zakariya Osman <61260744+Zakariya-Osman@users.noreply.github.com>
2025-05-27 10:31:33 -0600

    Update main script to save repsonse to json file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 from scraping.kijiji_scraper import fetch_search_html
 
 fetch_search_html("Calgary", "Bikes", 1)
-# print(html[:100])
--- scraping/kijiji_scraper.py
+++ scraping/kijiji_scraper.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -10,20 +11,17 @@ headers = {
     )
 }
 
-
 def fetch_search_html(city, keyword, page:int):
+
+    currentTime = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     results = []
-    #url = f'https://www.kijiji.ca/b-{city}/{keyword}/page-{page}//k0l1700199?address={city}%2C%20AB&dc=true&ll=51.04473309999999%2C-114.0718831&radius=50.0&view=list'
     url = f'https://www.kijiji.ca/b-buy-sell/{city}/{keyword}/page-{page}/k0c10l1700199?address={city}%2C%20AB&ll=51.04473309999999%2C-114.0718831&radius=50.0&sort=dateDesc&view=list'
     response = requests.get(url, headers=headers)
-
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Example: data = json.loads(script.string)
     for script in soup.find_all("script", {"type": "application/ld+json"}):
-        data = json.loads(script.string)
-
         try:
+            data = json.loads(script.string)
             for item in data["itemListElement"]:
                 product = item["item"]
                 listing = {
@@ -35,17 +33,7 @@ def fetch_search_html(city, keyword, page:int):
                 }
                 results.append(listing)
         except Exception as e:
-            continue  # Some script tags are not product listings
-
-    # Now 'results' contains all the listings!
-    print(f"Found {len(results)} listings")
-    for l in results:
-        print(l["title"], "|", l["price"])
-        print(l["url"])
-        print()
-
-
-    
-    # with open("data/raw_search.html", "w", encoding="utf-8") as f:
-    #     f.write(response.text)
-    # return response.text
+            continue
+    with open(f'data/cache/{currentTime}-page-{page}-raw.json', "w", encoding="utf-8") as f:
+        json.dump(results, f, ensure_ascii=False, indent=2)
+    return results
